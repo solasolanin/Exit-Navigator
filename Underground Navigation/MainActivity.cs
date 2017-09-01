9c90d6dd443085776d7893c70266f457869d22b6
Hidetaka Asaoka <solasolanin@outlook.jp>
2017-09-01 23:29:55 +0900

    プロジェクト ファイルを追加します。

---
+++ Underground Navigation/MainActivity.cs
@@ -0,0 +1,127 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using Android.Runtime;
+using Android.Locations;
+using System;
+using System.Linq;
+using Android.Content;
+
+namespace Underground_Navigation
+{
+    [Activity(Label = "Underground_Navigation", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, ILocationListener
+
+    {
+        private Location currentLocation;
+        private LocationManager locationManager;
+        private string locationProvider;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            
+            //Main.axmlにセット
+            SetContentView (Resource.Layout.Main);
+            InitializeLocationManager();
+
+            //ボタンが押されたらcameraViewを起動
+            var button1 = FindViewById<Button>(Resource.Id.cameraMode);
+            button1.Click += (_, __) =>
+            {
+                var intent = new Intent(this, typeof(cameraView));
+                StartActivity(intent);
+            };
+        }
+
+
+        private void InitializeLocationManager()
+        {
+            locationManager = (LocationManager)GetSystemService(LocationService);
+            var criteriaForLocationService = new Criteria
+            {
+                Accuracy = Accuracy.Fine
+                //FineならGPSで測位
+            };
+
+            //criteriaForLocationServiceの利用
+            var acceptableLocationProviders = locationManager.GetProviders(criteriaForLocationService, true);
+
+            //ロケーションプロバイダの使用確認
+            if (acceptableLocationProviders.Any())
+            {
+                //acceptableLocationProvidersに何かクエリが入っていたら
+                // 最初のクエリを取り出す
+                locationProvider = acceptableLocationProviders.First();
+            }
+            else
+            {
+                locationProvider = string.Empty;
+                Toast.MakeText(this, "Empty", ToastLength.Long).Show();
+            }
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            // アクティビティがレジュームされたら
+            // 位置情報を更新してくれるように依頼する
+
+            locationManager.RequestLocationUpdates(locationProvider, 0, 0, this);
+            Toast.MakeText(this, locationProvider.ToUpper() + "位置情報更新待ち",ToastLength.Long).Show();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+
+            // アクティビティが停止されたら
+            // 位置情報更新しないように依頼
+
+            locationManager.RemoveUpdates(this);
+        }
+
+
+        //ILocationListener
+
+        //位置情報が更新された時の処理
+        public void OnLocationChanged(Location location)
+        {
+            currentLocation = location;
+            if (currentLocation == null)
+            {
+                Toast.MakeText(this,"位置を特定できません", ToastLength.Long).Show();
+            }
+            else
+            {
+                Toast.MakeText(this, currentLocation.Latitude +"\n" + currentLocation.Longitude, ToastLength.Long).Show();
+                locationManager.RemoveUpdates(this);
+            }
+        }
+
+        //Location Providerが無効
+        public void OnProviderDisabled(string provider)
+        {
+
+        }
+
+        //Location Providerが有効
+        public void OnProviderEnabled(string provider)
+        {
+            
+        }
+
+        //Location Providerのステータスが変更
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
+        {
+            Toast.MakeText(this, provider + "\n" + status, ToastLength.Long).Show();
+        }
+    }
+
+    
+    
+
+}
+
---
+++ Underground Navigation/cameraView.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Hardware;
+using Xamarin.Media;
+using System.Threading.Tasks;
+using Android.Content;
+using Android.Graphics;
+using Tesseract;
+using Tesseract.Droid;
+using TiniyIoC;
+using XLabs.Ioc;
+
+namespace Underground_Navigation
+{
+    [Activity(Label = "cameraView")]
+    public class cameraView : Activity
+    {
+        
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.cameraView);
+
+            //cameraを起動
+            var picker = new MediaPicker(this);
+            if (!picker.IsCameraAvailable)
+                Console.WriteLine("No camera!");
+            else
+            {
+                var intent = picker.GetTakePhotoUI(new StoreCameraMediaOptions
+                {
+                    Name = "test.jpg",
+                    Directory = "MediaPickerSample"
+                });
+                StartActivityForResult(intent, 1);
+            }
+            var container = TinyIoCContainer.Current;
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            // User canceled
+            if (resultCode == Result.Canceled)
+                return;
+
+            //Imageの表示
+            data.GetMediaFileExtraAsync(this).ContinueWith(t => {
+                ImageView imageView = FindViewById<ImageView>(Resource.Id.imageView1);
+                imageView.SetImageBitmap(BitmapFactory.DecodeFile(t.Result.Path));
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+
+
+            //テキストファイルのパス
+            string langPath = @"MediaPickerSample/tmp";
+
+            //テキストの言語
+            string lngStr = "eng";
+
+
+            ITesseractApi api = new ITesseractApi(Context);
+                
+        }
+    }
+}
